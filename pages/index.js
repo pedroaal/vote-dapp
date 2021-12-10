88a3c9485d686a858665bc31e5236d970727ba93
PedroAltamirano <pedroaal04@gmail.com>
2021-12-10 16:43:09 -0500

    start index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,32 @@
 import Head from 'next/head'
+import Web3Container from '../util/Web3Container'
+import { useState } from 'react';
+
+function Dapp({ web3, accounts, contract }) {
+  const [balance, setbalance] = useState()
+  const [ethBalance, setEthBalance] = useState()
+
+  getStats = async () => { }
+
+  vote = async () => {
+    // const balanceInWei = await web3.eth.getBalance(accounts[0])
+  };
+
+  (
+    <>
+      <div class="form-group">
+        <label for="proposal">Candidatos</label>
+        <select class="form-control" name="proposal" id="proposal">
+          <option value='1'>Candidato 1</option>
+          <option value='2'>Candidato 2</option>
+          <option value='3'>Candidato 3</option>
+        </select>
+      </div>
+
+      <button type="button" name="vote" id="vote" class="btn btn-primary" onClick={vote}>Votar</button>
+    </>
+  )
+}
 
 export default function Home() {
   return (
@@ -9,14 +37,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div class="form-group">
-        <label for="proposal">Candidatos</label>
-        <select class="form-control" name="proposal" id="proposal">
-          <option value='1'>Candidato 1</option>
-          <option value='2'>Candidato 2</option>
-          <option value='3'>Candidato 3</option>
-        </select>
-      </div>
+      <Web3Container
+        renderLoading={() => <div>Loading Dapp Page...</div>}
+        render={({ web3, accounts, contract }) => (
+          <Dapp web3={web3} accounts={accounts} contract={contract} />
+        )}
+      />
     </div>
   )
 }
