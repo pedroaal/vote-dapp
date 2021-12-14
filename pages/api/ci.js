3b35137728a73ae024fccae7fa5ef7c594e5e476
PedroAltamirano <pedroaal04@gmail.com>
2021-12-14 15:21:18 -0500

    added ci validation endpoint

---
+++ pages/api/ci.js
@@ -0,0 +1,24 @@
+import Civil from '../../util/db'
+
+const validateCi = ci => {
+  return false
+}
+
+export default async (req, res) => {
+  const { method } = req
+
+  switch (method) {
+    case ('GET'):
+      Civil
+        .findAll()
+        .then(all => res.status(200).json(all))
+        .catch(error => console.log(error))
+      break
+    case ('POST'):
+      const { ci, fingerprint } = JSON.parse(req.body)
+      const person = await Civil.findOne({ where: { ci, fingerprint } })
+      const valid = person ? true : false
+      res.status(200).send(valid)
+      break
+  }
+}
