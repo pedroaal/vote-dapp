bef72778d5efbe3dec205ef8bb71a7a06413b9bc
PedroAltamirano <pedroaal04@gmail.com>
2021-12-13 22:17:42 -0500

    added pie chart

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,64 @@ import React, { useState, useEffect } from 'react';
 
 import { init, vote, getWinner, getStats } from '../util/getWeb3';
 
-function Dapp() {
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const colors = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(255, 206, 86, 0.2)',
+  'rgba(75, 192, 192, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+  'rgba(255, 159, 64, 0.2)',
+]
+
+const borderColors = [
+  'rgba(255, 99, 132, 1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+]
+
+const dataset = {
+  label: '# de Votos',
+  data: [],
+  backgroundColor: [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+  ],
+  borderColor: [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+  ],
+  borderWidth: 1,
+};
+
+const Dapp = () => {
   const [state, setState] = useState({ proposal: '', ci: '' })
   const [winner, setWinner] = useState('')
-  const [stats, setStats] = useState([])
+  const [stats, setStats] = useState()
 
   const getStatsF = () => {
     getStats()
       .then(tx => {
-        console.log(tx)
-        setStats(tx)
+        const labels = tx.map(vote => vote[0])
+        const data = tx.map(vote => Number(vote[1]))
+        const datasets = [{
+          ...dataset,
+          data
+        }]
+        const statsData = {
+          labels,
+          datasets
+        }
+        setStats(statsData)
       })
       .catch(err => console.log(err))
   }
@@ -20,6 +68,9 @@ function Dapp() {
   useEffect(() => {
     init()
     getStatsF()
+    setInterval(() => {
+      getStatsF()
+    }, (10 * 1000)); // sec * millsecs
   }, [])
 
   const giveVote = () => {
@@ -52,9 +103,9 @@ function Dapp() {
       }
       {
         stats &&
-        stats.map(proposal => (
-          <p>{proposal[0]}: {proposal[1]}</p>
-        ))
+        <Doughnut
+          data={stats}
+        />
       }
       <div className="form-group">
         <label htmlFor="proposal">Candidatos</label>
